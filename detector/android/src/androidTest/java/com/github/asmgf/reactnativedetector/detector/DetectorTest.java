d9ae6a414dfd460ff24cb2063c92b9b713ef20a3
asmgf <asmgf@users.noreply.github.com>
2018-06-15 00:45:23 +0300

    Add corner detector

--- detector/android/src/androidTest/java/com/github/asmgf/reactnativedetector/detector/DetectorTest.java
+++ detector/android/src/androidTest/java/com/github/asmgf/reactnativedetector/detector/DetectorTest.java
@@ -1,18 +1,62 @@
 package com.github.asmgf.reactnativedetector.detector;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.test.InstrumentationRegistry;
+
 import org.junit.Test;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class DetectorTest {
     @Test
     public void test_emptyMat_notDetected() {
         Detector detector = new Detector();
-        Mat emptyMat = new Mat();
+        Mat emptyMat = new Mat(0, 0, CvType.CV_8UC4);
 
         boolean detected = detector.detect(emptyMat);
 
         assertFalse(detected);
+    }
+
+    @Test
+    public void test_fullText_detected() throws Exception {
+        Detector detector = new Detector();
+        Mat mat = getMatFromAssets("text-full.jpg");
+
+        boolean detected = detector.detect(mat);
+
+        assertTrue(detected);
+    }
+
+    @Test
+    public void test_croppedText_notDetected() throws Exception {
+        Detector detector = new Detector();
+        Mat mat = getMatFromAssets("text-cropped.jpeg");
+
+        boolean detected = detector.detect(mat);
+
+        assertFalse(detected);
+    }
+
+    private static Bitmap getBitmapFromAssets(String name) throws IOException {
+        Context context = InstrumentationRegistry.getContext();
+        try (InputStream stream = context.getAssets().open(name)) {
+            return BitmapFactory.decodeStream(stream);
+        }
+    }
+
+    private static Mat getMatFromAssets(String name) throws IOException {
+        Mat mat = new Mat();
+        Utils.bitmapToMat(getBitmapFromAssets(name), mat);
+        return mat;
     }
 }
--- detector/native/src/Detector.cpp
+++ detector/native/src/Detector.cpp
@@ -3,7 +3,15 @@
 namespace asmgf {
     namespace detector {
         bool Detector::detect(cv::Mat& rgbaMat) {
-            return false;
+            static const int threshold = 15;
+
+            cv::Mat grayMat;
+            cv::cvtColor(rgbaMat, grayMat, CV_RGBA2GRAY);
+
+            std::vector<cv::Point> corners;
+            cv::goodFeaturesToTrack(grayMat, corners, threshold, 0.01, 10);
+
+            return corners.size() >= threshold;
         }
     }
 }
